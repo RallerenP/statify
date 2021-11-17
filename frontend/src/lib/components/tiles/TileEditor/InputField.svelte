dcc5b02928179185443c014593439360e759b1d6
Andreas Møller Laursen <Fyziikme@gmail.com>
2021-11-17 23:16:31 +0100

    Added modal settings & preview

---
+++ frontend/src/lib/components/tiles/TileEditor/InputField.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let value;
+
+</script>
+
+<label class="input-group my-2">
+    <span class="bg-primary w-[75px]"><slot></slot></span> 
+    <input type="text" {value} on:keyup={ (e) => value = e.target.value } class="input input-bordered w-3/5">
+</label>
--- frontend/src/lib/components/tiles/TileEditor/TileEditor.svelte
+++ frontend/src/lib/components/tiles/TileEditor/TileEditor.svelte
@@ -2,47 +2,36 @@
   import Modal from "../../Modal/Modal.svelte";
   import ModalButton from "../../Modal/ModalButton.svelte";
   import ModalTitle from "../../Modal/ModalTitle.svelte";
+  import InputField from "./InputField.svelte";
+  import TilePreview from "./TilePreview.svelte";
 
+  let title = 'default'
+  let dataSource = 'http://localhost:3000/random'
+  let data_3 = 'Something'
   export let open;
   export let create = false;
 
-  const tileSizeOptions = [
-    {
-      id: 0,
-      x: 1,
-      y: 1
-    },
-    {
-      id: 1,
-      x: 2,
-      y: 1
-    },
-    {
-      id: 2,
-      x: 1,
-      y: 2
-    },
-  ]
-
-  const tileTypes = [
-    'StatTile',
-    'ChartTile'
-  ]
 </script>
 
 <Modal class="max-w-[800px]" {open}>
   <ModalTitle>Create new tile</ModalTitle>
-  <div class="h-full w-full flex-grow bg-green-200">
-    <div class="flex bg-red-500">
-      <div class="flex-grow">
-        Settings
+  <div class="h-full w-full flex-grow">
+    <div class="flex">
+      <div class="w-1/2">
+        <span class="text-[18px]">Settings</span>
+        <div class="form-control p-2">
+          <InputField bind:value={title}>Title</InputField>
+          <InputField bind:value={dataSource}>Data Source</InputField>
+          <InputField bind:value={data_3}>Data 3</InputField>
+        </div> 
       </div>
-      <div class="flex-grow bg-green-500">
-        Preview
+      <div class="w-1/2">
+        <span class="text-[18px]">Preview</span>
+        <TilePreview {title} {dataSource} {data_3}></TilePreview>
       </div>
     </div>
-    <div class="bg-pink-500">
-      Tile Selector
+    <div class="">
+      <span class="text-[18px]">Tile Selector</span>
     </div>
   </div>
   <div class="w-full flex justify-end my-2">
@@ -53,57 +42,4 @@
       { create ? 'Create' : 'Update' }
     </ModalButton>
   </div>
-  
 </Modal>
-<!-- 
-    {#if !selectedTileOption}
-    <div class="flex">
-      <div class="max-w-{50}">
-        <p class="text-lg">Current tile data</p>
-        <p class="text-sm">Tile Size: <span class="text-primary">{tileSizeX} x {tileSizeY}</span></p>
-        <p class="text-sm">Tile Type: <span class="text-primary">{tileType}</span></p>
-        <p class="text-sm">Tile Label: <span class="text-primary">{tileLabel}</span></p>
-      </div>
-      <div class="flex-grow"></div>
-      <div class="w-6/12">
-        <p>Preview</p>
-        <StatTile
-        id={previewContent._id} 
-        gridOptions={{w: tileSizeX, h: tileSizeY, x: 0, y: 0}}
-        content={previewContent}
-        />
-      </div>
-    </div>
-    <div class="flex mt-10">
-      <div class="w-1/3 p-4"><img class="hover:border-primary hover:border-2 hover:cursor-pointer" src="{TileSize}" alt="" on:click={() => handleTileStyleClicked('size')}></div>
-      <div class="w-1/3 p-4"><img class="hover:border-primary hover:border-2 hover:cursor-pointer" src="{TileStyle}" alt="" on:click={() => handleTileStyleClicked('type')}></div>
-      <div class="w-1/3 p-4"><img class="hover:border-primary hover:border-2 hover:cursor-pointer" src="{TileLabel}" alt="" on:click={() => handleTileStyleClicked('label')}></div>
-    </div>
-    <div class="modal-action">
-      <div class="btn" on:click={() => deleteModalOpen = false}>Close</div>
-    </div>
-
-    {:else}
-    <p class="text-lg">Editing tile {selectedTileOption}</p>
-    {#if selectedTileOption === 'size'}
-      <div>
-        <ul>
-        {#each tileSizeOptions as tileSizeOption}
-          <li><button id={`${tileSizeOption.id}`} class="hover:bg-primary rounded" on:click={ (e) => handleClickedTileSize(e)}>{tileSizeOption.x} - {tileSizeOption.y}</button></li>
-        {/each}
-        </ul>
-      </div>
-    {:else if selectedTileOption === 'type'}
-      <div>
-        <ul>
-          {#each tileTypes as types}
-            <li><button id={`${types}`} class="hover:bg-primary rounded" on:click={ (e) => handleChangedTileType(e)}>- {types}</button></li>
-          {/each}
-        </ul>
-      </div>
-    {:else}
-      <div><input on:keypress="{(e) => handleKeypress(e)}" class="p-2 mx-1 rounded w-full input input-ghost" placeholder="New label for tile..."></div>
-    {/if}
-    <div class="btn btn-error mt-6" on:click={() => handleTileStyleClicked('')}>Back</div>
-
-    {/if} -->
