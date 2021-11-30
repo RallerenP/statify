5beaa020af3d86dc19ee37a4b12a16b2fe8ebd0e
RallerenP <17848169+RallerenP@users.noreply.github.com>
2021-11-30 14:19:58 +0100

    Add files

---
+++ frontend/src/lib/components/tiles/tileTypes/Chart.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { Chart } from "chart.js";
+  import { generateColors } from "./Utils"
+  import { onMount } from "svelte";
+
+  export let dataSource;
+  export let type: "bar" | "line" | "scatter" | "bubble" | "pie" | "doughnut" | "polarArea" | "radar"
+
+  let canvas;
+  let chart;
+  let data: any;
+  let options: any;
+
+  onMount(async () => {
+    data = await fetch(dataSource).then(res => res.json());
+
+    if (type === "line") {
+      options = { 
+        backgroundColor: 'rgb(75, 192, 192)', 
+        borderColor: 'rgb(75, 192, 192)',
+      };
+    }
+    
+    render();
+
+    
+  });
+
+  function render() {
+    if (chart !== undefined) chart.destroy();
+    chart = new Chart(canvas, {
+      type,
+      data: {
+        labels: data.value.labels,
+        datasets: [
+          {
+            backgroundColor: generateColors(data.value.labels.length),
+            data: data.value.numbers,
+            ...options
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    });
+  }
+
+</script>
+
+<canvas bind:this={canvas}></canvas>
