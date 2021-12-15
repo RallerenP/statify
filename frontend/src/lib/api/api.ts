a63973c02f9a30e21c6dc73ce4801efa7b86913f
RallerenP <17848169+RallerenP@users.noreply.github.com>
2021-12-15 13:06:53 +0100

    Added BACKEND_URL environment variable for frontend

--- frontend/src/lib/api/api.ts
+++ frontend/src/lib/api/api.ts
@@ -1,7 +1,7 @@
 import type { CreateMenuItemDTO } from "./dtos/MenuDTOs";
 import type { CreateTileDTO, TileDTO } from "./dtos/TileDTOs";
 
-export const base = process.env.STAGING ? 'http://statify-staging-api.rpovlsen.com' : `${window.location.protocol}//${window.location.hostname}:3000`;
+export const base = process.env.BACKEND_URL || 'http://localhost:3000';
 
 export const getAllMenus = async () => {
   return httpGet(`${base}/menu`).then(res => res.json());
